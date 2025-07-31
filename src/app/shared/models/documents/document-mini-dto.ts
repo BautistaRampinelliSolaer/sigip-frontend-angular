223af922c4702300e0f9bf09b180f250eb8ba794
Juan Bautista Rampinelli Guado <109321817+BautistaRampinelli@users.noreply.github.com>
2025-07-31 13:36:40 -0300

    Add models

---
+++ src/app/shared/models/documents/document-mini-dto.ts
@@ -0,0 +1,2 @@
+export interface DocumentMiniDTO {
+}
--- src/app/shared/models/documents/document.ts
+++ src/app/shared/models/documents/document.ts
@@ -1,2 +1,18 @@
+import { ObjectMiniDTO } from "../objects/object-mini-dto";
+import { ProjectMiniDTO } from "../projects/project-mini-dto";
+
 export interface Document {
+  id: number;
+  code: string;
+  name: string;
+  origin: string;
+  type: string;
+  careatedAt: Date;
+  folderPath: string;
+  milestone: string;
+  status: string;
+  executors: ObjectMiniDTO[];
+  responsible: ObjectMiniDTO;
+  reviewer: ObjectMiniDTO;
+  project: ProjectMiniDTO;
 }
