8936dcf601e82097e507dd5630a583aa64137a94
BautistaRampinelliSolaer <brampinelli.solaeringenieria@gmail.com>
2025-08-13 12:33:55 -0300

    Set generic config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,18 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { environment } from '../environments/environemnt.prod';
+import { ENV } from './core/config/environment-token';
+import { provideHttpClient, withFetch, withInterceptors, withRequestsMadeViaParent } from '@angular/common/http';
+import { authInterceptor } from './core/auth/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    {provide: ENV, useValue: environment},
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), 
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(withInterceptors([authInterceptor]), withFetch(), withRequestsMadeViaParent()),
   ]
 };
